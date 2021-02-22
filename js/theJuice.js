4b4334b9cbf529b6e92d727d5032315ec6693626
Shane Vincent <gamegamesgamers@gmail.com>
2021-02-22 13:19:32 -0800

    First Commit

---
+++ js/theJuice.js
@@ -0,0 +1,30 @@
+function linkDelay (URL) {
+    setTimeout( function() {window.location = URL}, 1500);
+    $('.cont').animate({
+        opacity: 0}, 1000, () => {
+            $('#parapap').remove();
+            $('.navBar').animate({
+                opacity: 1}, 500)
+            }
+        )
+}
+function fadeOut (URL) {
+    setTimeout( function() {window.location = URL}, 1000);
+    $('.contant').animate({
+        opacity: 0}, 1000)
+}
+function toPage (URL) {
+    setTimeout( function() {window.location = URL}, 1000);
+    $('.contant,.menu').animate({
+        opacity: 0}, 1000)
+}
+function fromPage (URL) {
+    setTimeout( function() {window.location = URL}, 1000);
+    $('.pageInfo,.pageContent,.musicContent,.musicInfo').animate({
+        opacity: 0}, 1000)
+}
+$('.contant,.pageInfo,.pageContent,.musicInfo,.musicContent').animate({
+    opacity: 1}, 1000)
+
+
+   
