c80fe24066546cfe65ab9d3de768b0fe3d8daeb7
Shane Vincent <gamegamesgamers@gmail.com>
2021-03-06 00:45:57 -0800

    hidetext

--- js/theJuice.js
+++ js/theJuice.js
@@ -25,23 +25,4 @@ function fromPage (URL) {
 }
 $('.contant,.pageInfo,.pageContent,.musicInfo,.musicContent').animate({
     opacity: 1}, 1000)
-
-(function() {
-    function scrollHorizontally(e) {
-        e = window.event || e;
-        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        document.getElementById('yourDiv').scrollLeft -= (delta * 40); // Multiplied by 40
-        e.preventDefault();
-    }
-    if (document.getElementById('yourDiv').addEventListener) {
-        // IE9, Chrome, Safari, Opera
-        document.getElementById('yourDiv').addEventListener('mousewheel', scrollHorizontally, false);
-        // Firefox
-        document.getElementById('yourDiv').addEventListener('DOMMouseScroll', scrollHorizontally, false);
-    } else {
-        // IE 6/7/8
-        document.getElementById('yourDiv').attachEvent('onmousewheel', scrollHorizontally);
-    }
-})();
-
    
