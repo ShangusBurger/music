7ff0d2010ab699ec875fb99861094c5c72b4cd7a
ShangusBurger <gamegamesgamers@gmail.com>
2022-10-27 21:23:03 -0700

    im gamer now

--- js/theJuice.js
+++ js/theJuice.js
@@ -26,5 +26,22 @@ function fromPage (URL) {
 $('.contant,.pageInfo,.pageContent,.musicInfo,.musicContent').animate({
     opacity: 1}, 1000)
 
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementsByClassName('scrollRow').scrollLeft -= (delta * 40); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.getElementsByClassName('scrollRow').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementsByClassName('scrollRow').addEventListener('mousewheel', scrollHorizontally, false);
+        // Firefox
+        document.getElementsByClassName('scrollRow').addEventListener('DOMMouseScroll', scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementsByClassName('scrollRow').attachEvent('onmousewheel', scrollHorizontally);
+    }
+})();
 
    
